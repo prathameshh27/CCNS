cff9fa0881d6045eee8807f36282de5a0bd95494
Prathamesh Pavnoji <prathameshh27@gmail.com>
2019-03-07 00:22:46 +0530

    playfair cipher: bugs fixed. fully functional.

--- src/PlayfairCipher.java
+++ src/PlayfairCipher.java
@@ -6,9 +6,6 @@ public class PlayfairCipher {
                              0,0,0,0,0,0,0,0,0,0,0,0,0};
     static char keyMatrix[][]=new char[5][5];
 
-    static int option;
-
-    //displays populated matrix
     public static void displayMatrix(){
         System.out.println();
         for(int i=0; i<5; i++) {
@@ -22,6 +19,7 @@ public class PlayfairCipher {
         System.out.println();
         for(int i=0; i<stateTracker.length; i++)
                 System.out.print(stateTracker[i]+"  ");
+        System.out.println();
     }
 
     //generates step 1 matrix using entered key
@@ -40,7 +38,6 @@ public class PlayfairCipher {
                 {
                     index =(int)key.charAt(counter)-(int)'a';
                     if(stateTracker[index]==0){
-                        //stateTracker[index]=key.charAt(counter);
                         stateTracker[index]=(i+1)*10+(j+1);
                         keyMatrix[i][j]=key.charAt(counter);
                     }
@@ -69,78 +66,82 @@ public class PlayfairCipher {
         }
     }
 
-    public static void playfairCipher(String message)
-    {
-        int char1Index, char2Index, rowChar1, colChar1, rowChar2, colChar2, temp;
-        char alteredChar1, alteredChar2;
+    public static int validateIndex(int index) {
+        if(index<4)
+            return index+1;
+        else return 0;
+    }
 
-        for(int i=0; i<message.length(); i=i+2)
-        {
+    public static void playfairCipher(String message) {
+        int char1Index, char2Index, rowIndexChar1, colIndexChar1, rowIndexChar2, colIndexChar2, temp;
+
+        for(int i=0; i<message.length(); i=i+2) {
             //determines where the char is located in matrix
                 temp=(int)message.charAt(i)-(int)'a';
-                char1Index=stateTracker[temp];
-                rowChar1=char1Index/10-1;
-                colChar1=char1Index%10-1;
+                char1Index=stateTracker[temp];          //for char 1
+                rowIndexChar1=char1Index/10-1;
+                colIndexChar1=char1Index%10-1;
 
                 temp=(int)'x'-(int)'a';
 
-                if(message.charAt(i)!= message.charAt(i+1))
-                    temp=(int)message.charAt(i+1)-(int)'a';
+                try{
+                    if(message.charAt(i)!= message.charAt(i+1))
+                        temp=(int)message.charAt(i+1)-(int)'a';
 
-                else
-                    i--;
+                    else
+                        i--;
+                }
 
-                char2Index=stateTracker[temp];
-                rowChar2=char2Index/10-1;
-                colChar2=char2Index%10-1;
+                catch (StringIndexOutOfBoundsException e){ }
+
+                char2Index=stateTracker[temp];          //for char 2
+                rowIndexChar2=char2Index/10-1;
+                colIndexChar2=char2Index%10-1;
 
 
             //chars with same rows/cols obey different rules
-            if(rowChar1==rowChar2)
-            {
-                colChar1=colChar1++ % 4;
-                colChar2=colChar2++ % 4;
+            if(rowIndexChar1==rowIndexChar2) {
+                colIndexChar1=validateIndex(colIndexChar1);
+                colIndexChar2=validateIndex(colIndexChar2);
+                System.out.println("same row: ");           //for debugging purpose
             }
 
-            else if(colChar1==colChar2)
-            {
-                rowChar1=rowChar1++ % 4;
-                rowChar2=rowChar2++ % 4;
+
+            else if(colIndexChar1==colIndexChar2) {
+                rowIndexChar1=validateIndex(rowIndexChar1);
+                rowIndexChar2=validateIndex(rowIndexChar2);
+                System.out.println("same col: ");           //for debugging purpose
             }
 
             //char with diff rows and cols
-            else
-            {
-                temp=colChar1;
-                colChar1=colChar2;
-                colChar2=temp;
+            else {
+                temp=colIndexChar1;
+                colIndexChar1=colIndexChar2;
+                colIndexChar2=temp;
+                System.out.println("diff row n col: ");     //for debugging purpose
             }
 
-            System.out.print(keyMatrix[rowChar1][colChar1]); //alters char1
-            System.out.print(keyMatrix[rowChar2][colChar2]); //alters char2
-
+            System.out.print(keyMatrix[rowIndexChar1][colIndexChar1]); //alters char1
+            System.out.println(keyMatrix[rowIndexChar2][colIndexChar2]); //alters char2
         }
     }
-
 
     public static void init(){
         String message, key;
         Scanner input = new Scanner(System.in);
 
-        //System.out.println("Press 1 for Encryption & 2 for Decryption");
-        //option = input.nextInt();
+        System.out.println("Enter key");
+        key = input.nextLine();
+
         System.out.println("Enter message");
         message = input.nextLine();
 
-        System.out.println("Enter key");
-        key = input.next();
-        //generates step1 matrix
-        genrateMatrix(key);
-        //prints keyMatrix
-        displayMatrix();
-        displayStateTracker();
-        //performs encryption / decryption
-        playfairCipher(message);
+        genrateMatrix(key);         //generates step1 matrix
 
+        //for debugging purpose
+        displayMatrix();            //prints keyMatrix
+        displayStateTracker();      //displays pos of each letter in mat
+
+        playfairCipher(message);    //performs encryption
     }
 }
